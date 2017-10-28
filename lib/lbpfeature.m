ce10936baa5642a4401da30b782974d765f241f5
sxuan2 <sijian.xuan@gmail.com>
2017-10-28 10:27:50 -0400

    ...

---
+++ lib/lbpfeature.m
@@ -0,0 +1,24 @@
+%%You gonna put folder 'images' in root directory
+function lbpfeature()
+conf.calDir = './' ; % calculating directory
+conf.dataDir = './images/' ; % data (image) directory 
+conf.outDir = './output/'; % output directory
+conf.prefix = 'lbp_' ;
+conf.lbpPath = fullfile(conf.outDir, [conf.prefix 'feature.mat']);
+
+imname = dir(strcat(conf.dataDir,'*.jpg'));    
+im_num = length(imname);
+lbp = zeros(im_num, 59);
+
+for a = 1:length(imname)
+    img = imread(fullfile(conf.dataDir,imname(a).name));
+    if (size(img,3)>1)% grayscale
+        img = rgb2gray(img);
+    lbp(a,:) = extractLBPFeatures(img);
+    sprintf('%s%d','image',a,'completed')
+end
+
+save(conf.lbpPath, 'lbp');
+csvwrite('./output/lbp.csv',lbp);
+end
+
