a146e34551613437649fa7fcf341b345287d0869
ChloeWsy <31856301+ChloeWsy@users.noreply.github.com>
2017-11-01 12:34:11 -0400

    Update for in-class use.R

--- doc/for in-class use.R
+++ doc/for in-class use.R
@@ -31,37 +31,44 @@ lbp.feature=read.csv("../data/lbp_feature.csv", header = F)
 hog.feature = read.csv("../data/hog_feature.csv")
 label=read.csv("../data/trainlabel.csv")
 ####Make dataset
-sift_data=data.frame(cbind(label,sift.feature[,-1]))
+####sift
+siftdata=data.frame(cbind(label,sift.feature[,-1]))
 test.index=sample(1:3000,500,replace=F)
-colnames(sift_data)[2]="y"
-sift_data = sift_data[,-1]
-test.sift=sift_data[test.index,]
-test.x.sift=test.sift[,-1]
-train.sift=sift_data[-test.index,]
+colnames(siftdata)[2]="y"
+siftdata = siftdata[,-1]
+test.sift = siftdata[test.index,]
+test.x.sift = test.sift[,-1]
+train.sift = siftdata[-test.index,]
+
+####lbp
 lbpdata = data.frame(cbind(label,lbp.feature))
 colnames(lbpdata)[2] = "y"
 lbpdata = lbpdata[,-1]
 test.lbp = lbpdata[test.index,]
 test.x.lbp = test.lbp[,-1]
 train.lbp = lbpdata[-test.index,]
+
+####hog
 hogdata = data.frame(cbind(label,hog.feature[,-1]))
 colnames(hogdata)[2] = "y"
 hogdata = hogdata[,-1]
 test.hog = hogdata[test.index,]
 test.x.hog = test.hog[,-1]
 train.hog = hogdata[-test.index,]
+
 ##############################################gbm model###之前先train好
-I need someone to write the train func of GBM.
+gbm.model <- train.gbm(train.sift)
 ###########################################train SVM
 svm.model <- train.svm(train.lbp)
 #######################################train Hog
 xgboost.model <- train.xgboost(train.hog)
 #######################################time usage
-I need someone to write the time of training GBM here
-e=system.time(svm <- train.svm(lbpdata))
+b = system.time(gbm <- train.gbm(siftdata))
+e = system.time(svm <- train.svm(lbpdata))
 g = system.time(xgboost <- train.xgboost(hogdata))
-time = list(svm=e, xgboost=g)
+time = list(gbm=b, svm=e, xgboost=g)
 print(time)
+
 ########################################train part finished, next we extract feature and do test, have error rate and time
 ###################################################在班里现场从这里开始！！！
 ##########################################TEST part begins here
@@ -90,14 +97,21 @@ colnames(hogdata)[2] = "y"
 hogdata = hogdata[,-1]##########################去掉label的名字
 test.hog = hogdata
 test.x.hog = test.hog[,-1]
+
 #####################################test SVM
 svm.pre=test.svm(svm.model,test.x.lbp)
 table(svm.pre,test.lbp$y)
+mean(svm.pre != test.lbp$y)
+
 ####################################test HoG
 xgboost.pre = test.xgboost(xgboost.model,test.hog)
 table(xgboost.pre, test.hog$y)
+mean(xgboost.pre != test.hog$y) 
+
 ########################################test GBM
-I need someone to write the error rate of GBM model here.
-########################################
+gbm.pre = test.gbm(gbm.model,test.x.sift)
+table(gbm.pre, test.sift$y)
+mean(gbm.pre != test.sift$y) 
+
 
 
