17b6dad237b0c77a7d56f7a88ad52d7f5fe301a0
Pinren Chen <pc2751@columbia.edu>
2017-11-01 12:17:04 -0400

    CNN model

--- lib/CNN.py
+++ lib/CNN.py
@@ -1,7 +1,9 @@
 '''
 Created on Oct 28, 2017
-'''
 
+@author: Pinren, Xiaoyu
+'''
+import cPickle as pickle
 from theano import tensor as T
 from theano.tensor.nnet import conv2d
 import theano
@@ -14,6 +16,7 @@ import numpy as np
 import pandas as pd
 from scipy.misc import imresize
 from scipy.misc import imread
+from scipy.misc import imshow
 import lasagne,theano,scipy
 import sys,pickle,os,re
 import theano.tensor as T
@@ -25,7 +28,7 @@ from scipy.misc import *
 from collections import OrderedDict
 from lasagne.init import Constant, GlorotUniform
 
-
+from lasagne.nonlinearities import leaky_rectify, softmax
 
 # pred=theano.function([x1,y1],outputs=prediction,on_unused_input='ignore')
 
@@ -34,33 +37,36 @@ from lasagne.init import Constant, GlorotUniform
 
 rng=np.random
 
-
-imagename=filter(lambda x:re.search(r".jpg",x),os.listdir("C:/Users/zjutc/Desktop/training_set/images")) # get all image names from folder
-y=pd.read_csv("C:/Users/zjutc/Desktop/training_set/label_train.csv")# get labels 
+dir_path = "C:/Users/zjutc/Desktop/"
+imagename=filter(lambda x:re.search(r".jpg",x),os.listdir(dir_path+"training_set/images")) # get all image names from folder
+imagename.sort(key=lambda f: int(filter(str.isdigit, f)))
+y=pd.read_csv(dir_path+"training_set/label_train.csv")# get labels
 
 df=pd.DataFrame(y)
 df["imagename"]=imagename
-df = df.sample(frac=1).reset_index(drop=True)  # shuffle 
+#df = df.sample(frac=1).reset_index(drop=True)  # shuffle 
 
 trainX=[]
 testX=[]
 
+# get trainX from all photos
+trainY=df.iloc[:2500,1]
 for i in df.loc[:2499,"imagename"]:
-    img=imread("C:/Users/zjutc/Desktop/training_set/images"+"/"+i)
+    img=imread(dir_path+"training_set/images"+"/"+i)
     if len(img.shape)==2:
         img_conv=np.zeros([img.shape[0],img.shape[1],3])
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
                 img_conv[i,j]=img[i,j]    
         img=img_conv
-    img=imresize(img,[128,128,3])
-    
+    img=imresize(img,[64,64,3])    
     trainX.append(img.transpose([2,0,1]))
     
-trainY=df.iloc[:2500,1]
 
+
+# get testX from all photos
 for i in df.loc[2500:,"imagename"]:
-    img=imread("C:/Users/zjutc/Desktop/training_set/images"+"/"+i)
+    img=imread(dir_path+"training_set/images"+"/"+i)
     if len(img.shape)==2:
         img_conv=np.zeros([img.shape[0],img.shape[1],3])
         for i in range(img.shape[0]):
@@ -68,7 +74,7 @@ for i in df.loc[2500:,"imagename"]:
                 img_conv[i,j]=img[i,j]
                 
         img=img_conv
-    img=imresize(img,[128,128,3])
+    img=imresize(img,[64,64,3])
     
     testX.append(img.transpose([2,0,1]))
 
@@ -80,58 +86,86 @@ testX=[x/256.0 for x in testX]
 trainY=trainY.astype("int64")
 testY=testY.astype("int64")
 
-
+trainX=np.asarray(trainX)
+trainY=np.asarray(trainY)
+testX=np.asarray(testX)
+testY=np.asarray(testY)
 
 ### start to build the CNN network
-x1=T.tensor4('x1',dtype='float64')
-y1=T.vector('y1',dtype='int64')
-batchsize=100
-network=InputLayer(shape=(None,3,128,128),input_var=x1)
-network=Conv2DLayer(network,48,(5,5),nonlinearity=very_leaky_rectify,W=GlorotUniform('relu'))
-network=MaxPool2DLayer(network,(2,2))
-network=Conv2DLayer(network,64,(5,5),nonlinearity=very_leaky_rectify,W=GlorotUniform('relu'))
-network=MaxPool2DLayer(network,(2,2))
-network=Conv2DLayer(network,96,(5,5),nonlinearity=very_leaky_rectify,W=GlorotUniform('relu'))
-network=MaxPool2DLayer(network,(3,3))
-network=DenseLayer(network,512,nonlinearity=very_leaky_rectify,W=lasagne.init.GlorotNormal())
 
-network=DenseLayer(network,2,nonlinearity=softmax)
+input_var = T.tensor4('X',dtype='float64')
+target_var = T.vector('y',dtype='int64')
+batchsize=20
+network = lasagne.layers.InputLayer((None, 3, 64, 64), input_var)
+network = lasagne.layers.Conv2DLayer(network, 32, (3, 3),
+                                     nonlinearity=leaky_rectify)
+network = lasagne.layers.Pool2DLayer(network, (3, 3), stride=2, mode='max')
+                                     
+network = lasagne.layers.Conv2DLayer(network, 64, (3, 3),
+                                     nonlinearity=leaky_rectify)
 
-rate=theano.shared(.0002)
-params = lasagne.layers.get_all_params(network)
+                                     
+                                     
+network = lasagne.layers.Pool2DLayer(network, (3, 3), stride=2, mode='max')
+
+network = lasagne.layers.Conv2DLayer(network, 96, (3, 3),
+                                     nonlinearity=leaky_rectify)
+network = lasagne.layers.Pool2DLayer(network, (3, 3), stride=2, mode='max')
+
+network = lasagne.layers.DenseLayer(lasagne.layers.dropout(network, 0.5),
+                                    128, nonlinearity=leaky_rectify,
+                                    W=lasagne.init.Orthogonal())
+network = lasagne.layers.DenseLayer(lasagne.layers.dropout(network, 0.5),
+                                    3, nonlinearity=softmax)    
+
+# create loss function
 prediction = lasagne.layers.get_output(network)
-loss = lasagne.objectives.categorical_crossentropy(prediction,y1)
-loss = loss.mean()
-updates_sgd = adagrad(loss, params, learning_rate=rate)
-updates = apply_nesterov_momentum(updates_sgd, params, momentum=0.9)
+loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+loss = loss.mean() + 1e-4 * lasagne.regularization.regularize_network_params(
+        network, lasagne.regularization.l2)
+
+# create parameter update expressions
+params = lasagne.layers.get_all_params(network, trainable=True)
+updates = lasagne.updates.rmsprop(loss, params, learning_rate=0.001)
+
+# compile training function that updates parameters and returns training loss
+train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
 
-train_model = theano.function([x1,y1],outputs=loss,updates=updates)
 
-pred = theano.function([x1,y1],outputs=lasagne.objectives.categorical_crossentropy(prediction,y1))
+# train network (assuming you've got some training data in numpy arrays)
+train_times=len(trainX)/batchsize
 
-### begin to train
-renewtrain=len(trainX)/batchsize
-renewtest=len(testX)/batchsize
-for i in range(15000):
-    if i>325 and i<3000:
-        rate.set_value(.001)
-    elif i>6500 and i<15000:
-        rate.set_value(.0005)            
-    i1=i%renewtrain
-    tindex=range(i1*batchsize,(i1+1)*batchsize)
-    newloss=train_model([trainX[i] for i in tindex],[trainY[i] for i in tindex])
-    print 'in %d round, the loss function is %f'%(i+1,newloss) 
-    if i%renewtrain==0:
-        tt1=range(500)
-        pred1=0.
-        tmp_x=testX[tt1]
-        tmp_y=testY[tt1]
-        pred1+=sum(pred(tmp_x,tmp_y))
-        print 'in %d circle, the total test error is %f'%(i/renewtest+1,pred1/250.0)
-        tmp=rng.permutation(2500)
-        trainX=trainX[tmp]
-        trainY=trainY[tmp]
+
+print("Training ...")
+
+for epoch in range(100):
+    loss = 0  
+    for i in range(train_times):  
+        
+        input_batch=trainX[i*batchsize:(i+1)*batchsize]
+        target_batch=trainY[i*batchsize:(i+1)*batchsize]
+
+
+        loss += train_fn(input_batch, target_batch)
+
+
+    
+    Net_FileName = 'training_set/CNNmodel.pkl'
+    netInfo = {'network': network, 'params': lasagne.layers.get_all_param_values(network)}
+    pickle.dump(netInfo, open(os.path.join(dir_path, Net_FileName), 'wb'),protocol=pickle.HIGHEST_PROTOCOL)
+    
+    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    predict_fn = theano.function([input_var], T.argmax(test_prediction, axis=1))
+
+    
+    prediction=predict_fn(testX)
+    prediction=np.asarray(prediction)
+    error=sum(prediction!=testY)/500.0
+    print "Epoch %d: Loss %g" % (epoch + 1, loss )
+    print "Epoch %d: Prediction Error %g" %(epoch+1,error)
+    
+
 
 
     
